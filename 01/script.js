9c32d4b020681401777f80a486bffc8e20b999ac
Oleg Nemanov <lego12239@yandex.ru>
2019-12-18 18:00:56 +0300

    script.js: добавил get_text()
    А уже из init() можно вызвать get_text().

--- 01/script.js
+++ 01/script.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", init)
 
 function init()
 {
+  get_text();
+}
+
+function get_text()
+{
   fetch("text.data")
     .then(response => response.text())
     .then(data => document.body.textContent = data)
     .catch(error => console.log(error))
 }
-
